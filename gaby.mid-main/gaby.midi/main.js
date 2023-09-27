46ab5a5ec9b0220097a768c4459314032a995acf
aikomiyamot9 <129952251+aikomiyamot9@users.noreply.github.com>
2023-09-27 08:45:32 -0300

    Add files via upload

---
+++ gaby.mid-main/gaby.midi/main.js
@@ -0,0 +1,66 @@
+/*tocar pom*/
+function tocarsompom (){
+    document.querySelector('#som_tecla_pom').play()
+}
+document.querySelector('.tecla_pom').onclick=tocarsompom;
+
+
+/*tocar clap*/
+function tocarsomclap (){
+    document.querySelector('#som_tecla_clap').play()
+}
+document.querySelector('.tecla_clap').onclick=tocarsomclap;
+
+
+/*tocar tim*/
+function tocarsomtim (){
+    document.querySelector('#som_tecla_tim').play()
+}
+document.querySelector('.tecla_tim').onclick=tocarsomtim;
+
+
+/*tocar puff*/
+function tocarsompuff (){
+    document.querySelector('#som_tecla_puff').play()
+}
+document.querySelector('.tecla_puff').onclick=tocarsompuff;
+
+
+/*tocar pom*/
+function tocarsomsplash (){
+    document.querySelector('#som_tecla_splash').play()
+}
+document.querySelector('.tecla_splash').onclick=tocarsomsplash;
+
+
+
+/*tocar pom*/
+function tocarsomtoim (){
+    document.querySelector('#som_tecla_toim').play()
+}
+document.querySelector('.tecla_toim').onclick=tocarsomtoim;
+
+
+
+/*tocar pom*/
+function tocarsompsh (){
+    document.querySelector('#som_tecla_psh').play()
+}
+document.querySelector('.tecla_psh').onclick=tocarsompsh;
+
+
+
+/*tocar pom*/
+function tocarsomtic (){
+    document.querySelector('#som_tecla_tic').play()
+}
+document.querySelector('.tecla_tic').onclick=tocarsomtic;
+
+
+
+/*tocar pom*/
+function tocarsomtom (){
+    document.querySelector('#som_tecla_tom').play()
+}
+document.querySelector('.tecla_tom').onclick=tocarsomtom;
+
